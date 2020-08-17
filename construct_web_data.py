d2af1a5528c8800d05630751724adc62f0fd36fc
DiLab <Dilab@DiLabui-MacBook-Pro.local>
2020-08-17 22:24:27 +0900

    .

--- construct_web_data.py
+++ construct_web_data.py
@@ -61,8 +61,8 @@ def preprocess(v):
         return v if v.strip() else None
 in_path_ls = [IN_DIR + in_path for in_path in sorted(os.listdir(IN_DIR))]
 for in_path in in_path_ls:
-    src_out_path = "../src-{}.txt".format(os.path.basename(in_path_ls))
-    tgt_out_path = "../tgt-{}.txt".format(os.path.basename(in_path_ls))
+    src_out_path = "../src-{}.txt".format(os.path.basename(in_path))
+    tgt_out_path = "../tgt-{}.txt".format(os.path.basename(in_path))
     with open(in_path, "r", encoding="utf8") as fp_read, \
         open(src_out_path, "r", encoding="utf8") as fp_write,\
         open(tgt_out_path, "r", encoding="utf8") as fp_write:
